48e013554c7a45d742f8a57aec5cbf330e44f5b4
mberrouk <mberrouk@student.1337.ma>
2024-02-04 20:41:17 +0100

    init

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,7 +14,7 @@ if [[ -z $(command -v nginx) ]]; then
 fi
 
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Hello world!\nThis is a test..." | sudo tee /data/web_static/releases/test/index.html
+printf "Hello world!\nThis is a test..." | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
