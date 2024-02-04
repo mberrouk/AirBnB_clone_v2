d1f27d5156241d97f62e5023a2bb032a13a3a496
mberrouk <mberrouk@student.1337.ma>
2024-02-04 18:50:13 +0100

    init

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,9 +2,20 @@
 # Install and configure a web server
 
 # Install Nginx if it not already installed
-if [[ -z $(which nginx) ]];
-then
-	sudo apt-get -y update
-	sudo apt-get -y install nginx
-	sudo ufw allow 'Nginx HTTP'
+if [[ -z $(which nginx) ]]; then
+	
+	apt-get -y update
+	apt-get -y install nginx
+	
+	if [[ -n $(which ufw) ]]; then
+		ufw allow 'Nginx HTTP'
+	fi
+
 fi
+
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "Hello world!\nThis is a test..." | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
